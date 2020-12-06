be02d2a63268b51e5b1821d089ba79b14b75d03e
Kacia Cheung <kaciac@bu.edu>
2020-12-05 21:40:03 -1000

    Preparing css for js

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,19 @@
 // Add your scripts here
+
+import ScrollMagic from 'scrollmagic/scrollmagic/uncompressed/ScrollMagic';
+import 'scrollmagic/scrollmagic/uncompressed/plugins/debug.addIndicators';
+
+var controller = new ScrollMagic.Controller();
+
+function stickyAnimation() {
+
+    var stickyHeader = new ScrollMagic.Scene({
+        triggerElement: "#trigger1", 
+        triggerHook: "onLeave",
+        duration: 300
+    })
+    .setPin("#pin1")
+    .addIndicators()
+    .addTo(controller);
+}
+
