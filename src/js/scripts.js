3e596a2ed25a543a60920d5de5db607f6c86d5f4
Kacia Cheung <kaciac@bu.edu>
2020-12-08 12:48:21 -1000

    fixed plugin

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,11 +1,14 @@
 // Add your scripts here
 
+
+import ScrollMagic from 'ScrollMagic';
+import 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js';
+import { TimelineMax } from 'gsap/all';
 import lottie from 'lottie-web';
-import ScrollMagic from 'scrollmagic';
 
 const controller = new ScrollMagic.Controller();
 
-// const tl = new TimelineMax();
+const tl = new TimelineMax();
 
 function stickyAnimation() {
   const stickyHeader = new ScrollMagic.Scene({
@@ -14,6 +17,7 @@ function stickyAnimation() {
     duration: '50%',
   })
     .setPin('#pin1', { pushFollowers: false })
+    //.addIndicators()
     .addTo(controller);
 
   // let fixHeader = new ScrollMagic.Scene({
@@ -32,20 +36,29 @@ function stickyAnimation() {
     // .setClassToggle('#pin1', 'test')
     .setTween(tl)
     .addTo(controller); */
+
+  const treeScene = new ScrollMagic.Scene({
+    triggerElement: '#treeTrigger',
+    triggerHook: 0,
+    duration: '1100%',
+  })
+    .setTween(tl)
+    .addIndicators()
+    .addTo(controller);
 }
 
-lottie.loadAnimation({
+const animation = lottie.loadAnimation({
   container: document.getElementById('treeLottie'),
   loop: true,
-  autoplay: true,
-  //path: 'https://gist.githubusercontent.com/sebasbarraud/b9229cf3b64d7b46d782f3742298b440/raw/544015d2b0e9dbef7eea10087f5f632f6303482c/carpeta.json',
+  autoplay: false,
+  // path: 'https://gist.githubusercontent.com/sebasbarraud/b9229cf3b64d7b46d782f3742298b440/raw/544015d2b0e9dbef7eea10087f5f632f6303482c/carpeta.json',
   path: 'https://gist.githubusercontent.com/ashleykolodziej/645a79568f0df30df09c6d1783a834fa/raw/577e21b4000bfe5248271e9a4f7e749cecab04f4/data.json',
-  //path: 'data.json',
+  // path: 'data.json',
 });
 
 stickyAnimation();
 
-/* function onAnimationDOMLoaded(e) {
+function onAnimationDOMLoaded(e) {
   tl.to({ frame: 0 }, 3, {
     frame: animation.totalFrames - 1,
     onUpdate() {
@@ -56,4 +69,3 @@ stickyAnimation();
 }
 
 animation.addEventListener('DOMLoaded', onAnimationDOMLoaded);
-*/
