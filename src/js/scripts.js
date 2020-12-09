370b03d8c005495e72ed629c5690b2fd767e5e9a
Kacia Cheung <kaciac@bu.edu>
2020-12-08 19:14:34 -1000

    sticky tree done

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,9 @@
 
 import lottie from 'lottie-web';
 
+import ScrollMagic from 'ScrollMagic';
+import 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js';
+
 const animation = lottie.loadAnimation({
   container: document.getElementById('treeLottie'),
   loop: true,
@@ -10,6 +13,21 @@ const animation = lottie.loadAnimation({
   path: 'https://gist.githubusercontent.com/ashleykolodziej/645a79568f0df30df09c6d1783a834fa/raw/577e21b4000bfe5248271e9a4f7e749cecab04f4/data.json',
   // path: 'data.json',
 });
+
+var controller = new ScrollMagic.Controller();
+
+function pintree () {
+  var pinHeading1 = new ScrollMagic.Scene({
+    triggerElement: '#trigger',
+    triggerHook: 0,
+    duration: '1100%'
+  })
+  .setPin('#treeLottie', {pushFollowers: false})
+  .addIndicators()
+  .addTo(controller)
+};
+
+pintree();
 
 const duration = document.body.clientHeight * 4;
 //console.log(document.body.clientHeight);
@@ -24,3 +42,4 @@ function animatelottie() {
   animation.goToAndStop((scrollPercentage) * 155);
 }
 document.addEventListener('scroll', animatelottie);
+
