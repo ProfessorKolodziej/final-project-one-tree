e1ca1af2d1477c34273250902f93b9f895889092
ashleykolodziej <kolodz10@bu.edu>
2020-12-10 14:16:33 -0500

    Resolves build errors, minor cleanup

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,36 +1,29 @@
 // Add your scripts here
 
 import lottie from 'lottie-web';
-
-import ScrollMagic from 'ScrollMagic';
-import 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js';
+import ScrollMagic from 'scrollmagic';
 
 const animation = lottie.loadAnimation({
   container: document.getElementById('treeLottie'),
   loop: true,
   autoplay: false,
-  // path: 'https://gist.githubusercontent.com/sebasbarraud/b9229cf3b64d7b46d782f3742298b440/raw/544015d2b0e9dbef7eea10087f5f632f6303482c/carpeta.json',
   path: 'https://gist.githubusercontent.com/ashleykolodziej/645a79568f0df30df09c6d1783a834fa/raw/577e21b4000bfe5248271e9a4f7e749cecab04f4/data.json',
-  // path: 'data.json',
 });
 
 const controller = new ScrollMagic.Controller();
 
 function pintree() {
-  const pinHeading1 = new ScrollMagic.Scene({
+  new ScrollMagic.Scene({
     triggerElement: '#trigger',
     triggerHook: 0,
     duration: '1100%',
   })
     .setPin('#treeLottie', { pushFollowers: false })
-  // .addIndicators()
     .addTo(controller);
 }
 
 pintree();
 
-const duration = document.body.clientHeight * 4;
-// console.log(document.body.clientHeight);
 function animatelottie() {
   // console.log(this.documentElement.scrollTop);
   const scrollPosition = this.documentElement.scrollTop;
